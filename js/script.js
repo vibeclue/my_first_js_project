c3464a79d1a2b02bbab90f32a0df9411d2aaad31
Artur <example@gmail.com>
2025-09-09 09:48:51 +0300

    add some checks

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?");
+// while (numberOfFilms === 0)
+//   numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?");
 
 const personalMovieDB = {
   count: numberOfFilms,
@@ -8,12 +10,23 @@ const personalMovieDB = {
   privat: false,
 };
 
-const prompt1 = prompt("Один из последних просмотренных фильмов?");
-const prompt2 = prompt("На сколько оцените его?");
-const prompt3 = prompt("Один из последних просмотренных фильмов?");
-const prompt4 = prompt("На сколько оцените его?");
+for (let i = 0; i < 2; i++) {
+  let prompt1;
+  let prompt2;
 
-personalMovieDB.movies[prompt1] = prompt2;
-personalMovieDB.movies[prompt3] = prompt4;
+  while (!prompt1 || !prompt2 || prompt1.length > 50) {
+    prompt1 = prompt("Один из последних просмотренных фильмов?");
+    prompt2 = prompt("На сколько оцените его?");
+  }
+
+  personalMovieDB.movies[prompt1] = prompt2;
+}
+
+if (personalMovieDB.count < 10)
+  console.log("Просмотрено довольно  мало фильмов");
+else if (10 <= personalMovieDB.count <= 30)
+  console.log("Вы классический зритель");
+else if (personalMovieDB.count > 30) console.log("Вы киноман");
+else console.log("Произошла ошибка");
 
 console.log(personalMovieDB);
